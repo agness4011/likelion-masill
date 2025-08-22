f3f109e1997382fdb98a210f943a575b64bea8b1
agness4011 <agness6565@skuniv.ac.kr>
2025-08-22 22:26:11 +0900

    **feat** PWA 설치

--- masill/vite.config.js
+++ masill/vite.config.js
@@ -1,9 +1,51 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: [
+        'favicons/favicon.ico',
+        'favicons/apple-touch-icon.png',
+        'favicons/favicon-96x96.png',
+        'favicons/favicon.svg'
+      ],
+      manifest: {
+        id: '/',
+        name: 'Masill',
+        short_name: 'Masill',
+        description: 'Masill PWA App',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: 'favicons/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'favicons/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   define: {
     global: "globalThis",
   },
